a202d30997f258aacdf06be97fd621da5dfa8755
Crisktina <crishidalgolopez@gmail.com>
2023-01-24 22:55:42 +0100

    first commit

--- src/Store.php
+++ src/Store.php
@@ -9,12 +9,17 @@ class Store
 
     public function increase($num){
         //Escribe tu código aquí...
+        echo $num++;
     }
 
     //Ejercicio 2 -> escribe una función llamada decrease() que reste 1 a un valor dado. Cuando llegue a 0 se quedará en 0 y no bajará a números negativos.
     //Exercise 2 -> write a function called decrease() that subtracts 1 from a given value. When it reaches 0 it will stay at 0 and will not go down to negative numbers.
     
     //Escribe tu código aquí...
+    public function decrease($num){
+        //Escribe tu código aquí...
+        echo $num--;
+    }
 
     //Ejercicio 3 -> escribe una función llamada rateByPrice() que devuelva el signo € si el valor es de 0 a 50, devuelva €€ si el valor es de 50.01 a 100 y devuelva €€€ si el valor es de 100.01 para arriba.
     //Exercise 3 -> write a function called rateByPrice() that returns the € sign if the value is from 0 to 50, returns €€ if the value is from 50.01 to 100 and returns €€€ if the value is 100.01 for up.
