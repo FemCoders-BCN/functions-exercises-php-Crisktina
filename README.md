b25bf6d62b9bc33180cca27dee7d0833145a1b72
Crisktina <crishidalgolopez@gmail.com>
2023-01-26 20:23:52 +0100

    install composer

--- README.md
+++ README.md
@@ -3,38 +3,46 @@
 ## ES
 
 ### Introducción
+
 Este es un ejercicio básico de PHP para aprender a hacer funciones, empleando condicionales, bucles y operadores. Dentro de la carpeta src se encuentran 3 archivos con una serie de instrucciones que te permitirá pasar los tests que se encuentran en la carpeta tests, dentro de cada archivo de test están las aserciones que queremos que pasen cuando se ejecuten las funciones que tú vas a escribir. Será necesario que instales phpunit con composer para poder ejecutar el script que te permitirá correr el test de cada archivo. Pasa el test y mira la consola que te dirá cuál es el error, y así, puedas escribir correctamente tu código. El objetivo será que puedas pasar los tests de los 3 archivos.
 
 ### Instrucciones
+
 1. git clone <repositorio> (el proyecto deberá estar dentro de la carpeta htdocs de xampp o mamp).
 2. Para instalar phpUnit: composer require --dev phpunit/phpunit
 3. Para instalar composer: composer i
-4. Para correr el test: ./vendor/bin/phpunit tests/NombreDelArchivoDelTest.php
+4. Para correr el test: ./vendor/bin/phpunit tests/FunctionsTest.php
+   ./vendor/bin/phpunit tests/ItemTest.php
+   ./vendor/bin/phpunit tests/StoreTest.php
 5. Para este ejercicio no será necesario usar un servidor, por lo que no se requiere tener xampp o mamp encendido para hacer correr los tests, sin embargo, hay un archivo index.php con información en la que te puedes apoyar, por lo que tener el servidor encendido es requerido.
 
-
 ### Reglas
+
 1. Empieza con el archivo Functions.php, una vez lo tengas terminado pasa al archivo Store.php y finalmente completa el ejercicio del archivo Item.php
 2. Solo se debe escribir la función dentro de los archivos de la carpeta src.
 3. Puedes mirar lo que hay en los archivos de la carpeta tests pero no podrás modificarlos.
 4. Se deberán usar parámetros.
 5. Deberás ir mirando lo que devuelve la consola cada vez que corras los tests.
 6. El objetivo final es que pasen todos los tests de los 3 archivos.
+
 ---
 
 ## EN
 
 ### Introduction
+
 This is a basic PHP exercise to learn how to make functions, using conditionals, loops, and operators. Inside the src folder there are 3 files with a series of instructions that will allow you to pass the tests found in the tests folder, inside each test file are the assertions that we want to pass when the functions that you write are executed. You will need to install phpunit with composer in order to run the script that will allow you to run the test for each file. Pass the test and see the console that will tell you what the error is, so you can write your code correctly. The objective will be that you can pass the tests of the 3 files.
 
 ### Instructions
-1. git clone <repository> (the folder has to be inside the htdocs folder of xampp or mamp).  
+
+1. git clone <repository> (the folder has to be inside the htdocs folder of xampp or mamp).
 2. To install phpUnit: composer require --dev phpunit/phpunit
 3. To install composer: composer i
 4. To run a test: ./vendor/bin/phpunit tests/TestFileName.php
 5. For this exercise it will not be necessary to use a server, so it's not required to have xampp or mamp turned on for running the tests, however, there is an index.php file with information you can rely on, so having the server turned on is required.
 
 ### Rules
+
 1. Start with the Functions.php file, once you have finished it move on to the Store.php file and finally complete the Item.php file exercise.
 2. The function should only be written inside the files in the src folder.
 3. You can see what's in the files in the tests folder but you shouldn't modify them.
--- src/Functions.php
+++ src/Functions.php
@@ -8,46 +8,77 @@ class Functions
     //Exercise 1 -> Write a function called sum() that returns the sum of 2 given numbers.
 
     public function sum($num1, $num2){
-        //Escribe tu código aquí
+        return $num1 + $num2;
     }
 
     //Ejercicio 2 -> escribe una función llamada reverse() que cambie a orden inverso los caracteres de un string dado.
     //Exercise 2 -> write a function called reverse() that reverses the characters in a given string.
     
-    //Escribe tu código aquí...
+    public function reverse($string){
+        return strrev($string);
+    }
 
     //Ejercicio 3 -> escribe una función llamada eachCharToArray() que convierta un string en un array donde cada elemento del array corresponda a un caracter del string dado, se debe eliminar los espacios vacios.
     //Exercise 3 -> write a function called eachCharToArray() that converts a string into an array where each element of the array corresponds to a character of the given string, empty spaces must be removed.
     
     //Escribe tu código aquí...
+    public function eachCharToArray($string){
+        // array_splice() Removes and replaces specified elements of an array
+        return $string;
+    }
 
     //Ejercicio 4 -> escribe una función llamada eachWordToArray() que convierta un string en un array donde cada elemento del array corresponda a una palabra del string dado, se deben eliminar los elementos vacios.
     //Exercise 4 -> write a function called eachWordToArray() that converts a string to an array where each element of the array corresponds to a word of the given string, empty elements must be removed.
     
-    //Escribe tu código aquí...
+
+    public function eachWordToArray($string){
+        return $string;
+    }
 
     //Ejercicio 5 -> escribe una función llamada isBoolean() que devuelva si un valor dado es booleano o no.
     //Exercise 5 -> write a function called isBoolean() that returns whether a given value is boolean or not.
     
-    //Escribe tu código aquí...
+    public function isBoolean($something){
+        return is_bool($something);
+    }
 
     //Ejercicio 6 -> escribe una función llamada addItem() y añade pastelito a un array dado.
     //Exercise 6 -> write a function called addItem() and add 'pastelito' to a given array.
     
-    //Escribe tu código aquí...
+    public function addItem($array){
+        $result = array_push($array, "pastelito");
+        return $result;
+    }
 
     //Ejercicio 7 -> escribe una función llamada updateItem() y cambia el valor de pastelito por tarta.
     //Exercise 7 -> write a function called updateItem() and change the value of 'pastelito' to 'tarta'.
     
-    //Escribe tu código aquí...
+    public function updateItem($array){
+        $find_index = array_search("pastelito", $array);
+        $array[$find_index] = "tarta";
+        // if (in_array("pastelito", $array)){
+        //     $solution = array_replace($array [])
+        // };
+        // return $solution;
+    }
 
     //Ejercicio 8 -> escribe una función llamada deleteItem() que elimine helado del array dado porque me lo comí y ya no lo quiero en la lista. Mira que las claves del array queden ordenadas.
     //Exercise 8 -> write a function called deleteItem() that removes 'helado' from the given array because I ate it and don't want it in the list anymore. See that the keys of the array are ordered.
     
-    //Escribe tu código aquí...
+
+    public function deleteItem($array){
+        
+        asort($array); // Sorts an associative array in ascending order, according to the value
+        arsort($array); // Sorts an associative array in descending order, according to the value
+    }
 
     //Ejercicio 9 -> escribe una función llamada joinMeals() que junte 2 arrays dados para completar mi comida.
     //Exercise 9 -> write a function called joinMeals() that joins 2 given arrays to complete my meal.
    
-    //Escribe tu código aquí...
+
+    public function joinMeals(){
+
+        
+    }
 }
+var_dump(Functions);
